94cccbe2ec2a395e79c7b9c3fe0e2c0bfb7b8253
Marivel Muñoz <marivelmunozm@gmail.com>
2023-02-16 15:54:36 -0500

    crecion carpeta assets

--- lib/pantallas/pagina_splash.dart
+++ lib/pantallas/pagina_splash.dart
@@ -8,6 +8,20 @@ class PaginaSplash extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFFFFF7E9),
       appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 81, top: 271),
+            ),
+            SizedBox(
+              width: 250,
+              height: 120.04,
+              child: Image.asset('assets/bandera.png'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
