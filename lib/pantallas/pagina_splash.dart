a6fb2be5d2b8b84e330e4bcb133c6733387848d0
Marivel Muñoz <marivelmunozm@gmail.com>
2023-02-16 15:13:38 -0500

    creacion color de pantalla

---
+++ lib/pantallas/pagina_splash.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PaginaSplash());
+
+class PaginaSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFF7E9),
+      appBar: AppBar(),
+    );
+  }
+}
