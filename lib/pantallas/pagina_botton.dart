0895ac03260890762ef2716f001cf3eca71a4f85
Marivel Muñoz <marivelmunozm@gmail.com>
2023-02-17 08:45:27 -0500

    creacion de segun pagina

---
+++ lib/pantallas/pagina_botton.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hola/pantallas/pagina_splash.dart';
+
+class PaginaBotton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
--- lib/pantallas/pagina_splash.dart
+++ lib/pantallas/pagina_splash.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:hola/pantallas/pagina_botton.dart';
 
 void main() => runApp(PaginaSplash());
 
@@ -9,31 +10,35 @@ class PaginaSplash extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFF7E9),
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 271, left: 81),
-            ),
-            SizedBox(
-              width: 250,
-              height: 120.04,
-              child: Image.asset('assets/bandera.png'),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 20.96),
-            ),
-            Text(
-              "Clubnouse",
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontStyle: FontStyle.normal,
-                color: Color(0xF000000),
-                fontSize: 48,
+      body: InkWell(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: ((context) => PaginaBotton())));
+        },
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 271, left: 81),
               ),
-            ),
-          ],
+              SizedBox(
+                width: 250,
+                height: 120.04,
+                child: Image.asset('assets/bandera.png'),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20.96),
+              ),
+              Text(
+                "Clubnouse",
+                selectionColor: Color(0xF000000),
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontStyle: FontStyle.normal,
+                  fontSize: 48,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
