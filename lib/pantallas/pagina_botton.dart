e029c2b2dd606849b23e7d9cf2c2a8bc3c9fcdb5
Marivel Muñoz <marivelmunozm@gmail.com>
2023-02-17 12:01:32 -0500

    creacion de imagenes

--- lib/pantallas/pagina_botton.dart
+++ lib/pantallas/pagina_botton.dart
@@ -5,7 +5,30 @@ class PaginaBotton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Color(0xFFFFF7E9),
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.only(top: 38.25)),
+          Row(
+            children: [
+              Icon(
+                Icons.arrow_back,
+                color: Color(0xF000000),
+                size: 20,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Image.asset('assets/hombre.png A'),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
